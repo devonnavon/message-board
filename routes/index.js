6ef82a2cfb6f0c607101f2b6a0bf54bae467dc8f
Devon Navon <devon.navon@gmail.com>
2020-06-23 12:23:16 -0700

    board complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+	res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', function (req, res, next) {
+	var obj = {
+		text: req.body.message,
+		user: req.body.name,
+		added: new Date(),
+	};
+	messages.push(obj);
+	res.redirect('/');
 });
 
 module.exports = router;
